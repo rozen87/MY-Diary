02dce718fca1ac83dd95780a39b203df153fe55d
rozen87 <wpfha07@gmail.com>
2026-06-08 13:45:48 +0900

    font+update add

---
+++ my-app/app/fonts.ts
@@ -0,0 +1,13 @@
+// cspell:disable-next-line
+import { Inter, Zen_Kaku_Gothic_New } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+// cspell:disable-next-line
+export const zen = Zen_Kaku_Gothic_New({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-jp",
+});
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
+import { inter, zen } from "./fonts";
 import Header from "@/components/navigation/header";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("h-full", "antialiased", "font-mono")}>
+    <html lang="en" className={`${inter.variable} ${zen.variable}`}>
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
